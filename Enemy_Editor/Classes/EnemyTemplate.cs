5be1729412cc93cebb902f43669b39f980698181
Алексей Фоменко <genius690@mail.ru>
2024-10-18 01:59:15 +0700

    added interface

--- Enemy_Editor/Classes/EnemyTemplate.cs
+++ Enemy_Editor/Classes/EnemyTemplate.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Enemy_Editor.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Enemy_Editor.Classes
 {
-    public class EnemyTemplate
+    public class EnemyTemplate : IEnemyTemplateList
     {
         public string Name
         {
@@ -63,5 +64,10 @@ namespace Enemy_Editor.Classes
         double goldModifier;
         //Шанс на появление
         double spawnChance;
+
+        public void SaveToJson()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
