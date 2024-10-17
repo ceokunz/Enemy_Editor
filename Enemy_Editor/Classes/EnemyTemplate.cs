65cb9e28c5ff802e467476b39d2dec9a25cfb288
Алексей Фоменко <genius690@mail.ru>
2024-10-18 01:53:20 +0700

    Добавьте файлы проекта.

---
+++ Enemy_Editor/Classes/EnemyTemplate.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enemy_Editor.Classes
+{
+    public class EnemyTemplate
+    {
+        public string Name
+        {
+            get => name;
+            set => name = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public string IconName
+        {
+            get => iconName;
+            set => iconName = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public int BaseLife
+        {
+            get => baseLife;
+            set => baseLife = value;
+        }
+
+        public double LifeModifier
+        {
+            get => lifeModifier;
+            set => lifeModifier = value;
+        }
+
+        public int BaseGold
+        {
+            get => baseGold;
+            set => baseGold = value;
+        }
+
+        public double GoldModifier
+        {
+            get => goldModifier;
+            set => goldModifier = value;
+        }
+
+        public double SpawnChance
+        {
+            get => spawnChance;
+            set => spawnChance = value;
+        }
+
+        //Название противника
+        string name;
+        //Название иконки
+        string iconName;
+        //Атрибуты здоровья
+        int baseLife;
+        double lifeModifier;
+
+        //Атрибуты золота за победу над противником
+        int baseGold;
+        double goldModifier;
+        //Шанс на появление
+        double spawnChance;
+    }
+}
---
+++ Enemy_Editor/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace Enemy_Editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<IconItem> EnemiesList { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            EnemiesList = new List<IconItem>();
+
+            OpenFolderDialog choofdlog = new OpenFolderDialog();
+
+            if ((bool)choofdlog.ShowDialog())
+            {
+                LoadImages(choofdlog.FolderName);
+            }
+
+            IconListBox.ItemsSource = EnemiesList;
+        }
+
+
+        public void LoadImages(string path)
+        {
+            string filter = "*.png";
+            string[] files = Directory.GetFiles(path, filter);
+            foreach (string file in files)
+            {
+                EnemiesList.Add(new IconItem(file));
+            }
+        }
+    }
+
+    public class IconItem
+    {
+        public string IconPath { get; set; }
+        public string IconName { get; set; }
+
+        public IconItem(string iconPath)
+        {
+            IconPath = iconPath;
+
+            string[] m = iconPath.Split(new char[] { '\\' });
+
+            IconName = m.Last();
+        }
+    }
+}
